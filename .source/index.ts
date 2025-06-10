8e06cb1f588a8a458a55c032b058c297f147b746
Josef Bnm <ssefbnm@gmail.com>
2025-06-10 15:22:46 +0100

    reconfig-fumadocs

--- .source/index.ts
+++ .source/index.ts
@@ -1,233 +1,30 @@
 // @ts-nocheck -- skip type checking
-import * as docs_24 from '../content/docs/utils/use-animation-variants.mdx?collection=docs&hash=1748883387721';
-import * as docs_23 from '../content/docs/utils/set-stagger-direction.mdx?collection=docs&hash=1748883387721';
-import * as docs_22 from '../content/docs/text/text-vertical.mdx?collection=docs&hash=1748883387721';
-import * as docs_21 from '../content/docs/text/text-stagger-inview.mdx?collection=docs&hash=1748883387721';
-import * as docs_20 from '../content/docs/text/text-stagger-hover.mdx?collection=docs&hash=1748883387721';
-import * as docs_19 from '../content/docs/slideshow/slideshow.mdx?collection=docs&hash=1748883387721';
-import * as docs_18 from '../content/docs/section-gallery/section-gallery.mdx?collection=docs&hash=1748883387721';
-import * as docs_17 from '../content/docs/rating-stars/rating-stars.mdx?collection=docs&hash=1748883387721';
-import * as docs_16 from '../content/docs/heros/hero-rotated-gallery.mdx?collection=docs&hash=1748883387721';
-import * as docs_15 from '../content/docs/heros/hero-gradient.mdx?collection=docs&hash=1748883387721';
-import * as docs_14 from '../content/docs/custom-cursor/custom-cursor.mdx?collection=docs&hash=1748883387721';
-import * as docs_13 from '../content/docs/containers/grid-staggered.mdx?collection=docs&hash=1748883387721';
-import * as docs_12 from '../content/docs/containers/grid-bento.mdx?collection=docs&hash=1748883387721';
-import * as docs_11 from '../content/docs/containers/gallery-rotated-scroll.mdx?collection=docs&hash=1748883387721';
-import * as docs_10 from '../content/docs/containers/container-stagger.mdx?collection=docs&hash=1748883387721';
-import * as docs_9 from '../content/docs/containers/container-infinite-scroll.mdx?collection=docs&hash=1748883387721';
-import * as docs_8 from '../content/docs/containers/container-clipped.mdx?collection=docs&hash=1748883387721';
-import * as docs_7 from '../content/docs/cards/cards-stack.mdx?collection=docs&hash=1748883387721';
-import * as docs_6 from '../content/docs/cards/cards-stack-rotated.mdx?collection=docs&hash=1748883387721';
-import * as docs_5 from '../content/docs/cards/card-testimonial.mdx?collection=docs&hash=1748883387721';
-import * as docs_4 from '../content/docs/cards/card-hover-reveal.mdx?collection=docs&hash=1748883387721';
-import * as docs_3 from '../content/docs/cards/card-flip.mdx?collection=docs&hash=1748883387721';
-import * as docs_2 from '../content/docs/cards/card-curtain-reveal.mdx?collection=docs&hash=1748883387721';
-import * as docs_1 from '../content/docs/background/background-gradient.mdx?collection=docs&hash=1748883387721';
-import * as docs_0 from '../content/docs/index.mdx?collection=docs&hash=1748883387721';
-import { _runtime } from 'fumadocs-mdx';
-import * as _source from '../source.config';
-export const docs = _runtime.docs<typeof _source.docs>(
-  [
-    {
-      info: {
-        path: 'index.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/index.mdx',
-      },
-      data: docs_0,
-    },
-    {
-      info: {
-        path: 'background/background-gradient.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/background/background-gradient.mdx',
-      },
-      data: docs_1,
-    },
-    {
-      info: {
-        path: 'cards/card-curtain-reveal.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-curtain-reveal.mdx',
-      },
-      data: docs_2,
-    },
-    {
-      info: {
-        path: 'cards/card-flip.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-flip.mdx',
-      },
-      data: docs_3,
-    },
-    {
-      info: {
-        path: 'cards/card-hover-reveal.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-hover-reveal.mdx',
-      },
-      data: docs_4,
-    },
-    {
-      info: {
-        path: 'cards/card-testimonial.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-testimonial.mdx',
-      },
-      data: docs_5,
-    },
-    {
-      info: {
-        path: 'cards/cards-stack-rotated.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/cards-stack-rotated.mdx',
-      },
-      data: docs_6,
-    },
-    {
-      info: {
-        path: 'cards/cards-stack.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/cards/cards-stack.mdx',
-      },
-      data: docs_7,
-    },
-    {
-      info: {
-        path: 'containers/container-clipped.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-clipped.mdx',
-      },
-      data: docs_8,
-    },
-    {
-      info: {
-        path: 'containers/container-infinite-scroll.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-infinite-scroll.mdx',
-      },
-      data: docs_9,
-    },
-    {
-      info: {
-        path: 'containers/container-stagger.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-stagger.mdx',
-      },
-      data: docs_10,
-    },
-    {
-      info: {
-        path: 'containers/gallery-rotated-scroll.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/gallery-rotated-scroll.mdx',
-      },
-      data: docs_11,
-    },
-    {
-      info: {
-        path: 'containers/grid-bento.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/grid-bento.mdx',
-      },
-      data: docs_12,
-    },
-    {
-      info: {
-        path: 'containers/grid-staggered.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/containers/grid-staggered.mdx',
-      },
-      data: docs_13,
-    },
-    {
-      info: {
-        path: 'custom-cursor/custom-cursor.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/custom-cursor/custom-cursor.mdx',
-      },
-      data: docs_14,
-    },
-    {
-      info: {
-        path: 'heros/hero-gradient.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/heros/hero-gradient.mdx',
-      },
-      data: docs_15,
-    },
-    {
-      info: {
-        path: 'heros/hero-rotated-gallery.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/heros/hero-rotated-gallery.mdx',
-      },
-      data: docs_16,
-    },
-    {
-      info: {
-        path: 'rating-stars/rating-stars.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/rating-stars/rating-stars.mdx',
-      },
-      data: docs_17,
-    },
-    {
-      info: {
-        path: 'section-gallery/section-gallery.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/section-gallery/section-gallery.mdx',
-      },
-      data: docs_18,
-    },
-    {
-      info: {
-        path: 'slideshow/slideshow.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/slideshow/slideshow.mdx',
-      },
-      data: docs_19,
-    },
-    {
-      info: {
-        path: 'text/text-stagger-hover.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/text/text-stagger-hover.mdx',
-      },
-      data: docs_20,
-    },
-    {
-      info: {
-        path: 'text/text-stagger-inview.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/text/text-stagger-inview.mdx',
-      },
-      data: docs_21,
-    },
-    {
-      info: {
-        path: 'text/text-vertical.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/text/text-vertical.mdx',
-      },
-      data: docs_22,
-    },
-    {
-      info: {
-        path: 'utils/set-stagger-direction.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/utils/set-stagger-direction.mdx',
-      },
-      data: docs_23,
-    },
-    {
-      info: {
-        path: 'utils/use-animation-variants.mdx',
-        absolutePath:
-          '/Users/ycf/Documents/systaliko-ui/content/docs/utils/use-animation-variants.mdx',
-      },
-      data: docs_24,
-    },
-  ],
-  [],
-);
+import * as docs_25 from "../content/docs/utils/use-animation-variants.mdx?collection=docs&hash=1748883387721"
+import * as docs_24 from "../content/docs/utils/set-stagger-direction.mdx?collection=docs&hash=1748883387721"
+import * as docs_23 from "../content/docs/text/text-vertical.mdx?collection=docs&hash=1748883387721"
+import * as docs_22 from "../content/docs/text/text-stagger-inview.mdx?collection=docs&hash=1748883387721"
+import * as docs_21 from "../content/docs/text/text-stagger-hover.mdx?collection=docs&hash=1748883387721"
+import * as docs_20 from "../content/docs/story/story.mdx?collection=docs&hash=1748883387721"
+import * as docs_19 from "../content/docs/slideshow/slideshow.mdx?collection=docs&hash=1748883387721"
+import * as docs_18 from "../content/docs/section-gallery/section-gallery.mdx?collection=docs&hash=1748883387721"
+import * as docs_17 from "../content/docs/rating-stars/rating-stars.mdx?collection=docs&hash=1748883387721"
+import * as docs_16 from "../content/docs/heros/hero-rotated-gallery.mdx?collection=docs&hash=1748883387721"
+import * as docs_15 from "../content/docs/heros/hero-gradient.mdx?collection=docs&hash=1748883387721"
+import * as docs_14 from "../content/docs/custom-cursor/custom-cursor.mdx?collection=docs&hash=1748883387721"
+import * as docs_13 from "../content/docs/containers/grid-staggered.mdx?collection=docs&hash=1748883387721"
+import * as docs_12 from "../content/docs/containers/grid-bento.mdx?collection=docs&hash=1748883387721"
+import * as docs_11 from "../content/docs/containers/gallery-rotated-scroll.mdx?collection=docs&hash=1748883387721"
+import * as docs_10 from "../content/docs/containers/container-stagger.mdx?collection=docs&hash=1748883387721"
+import * as docs_9 from "../content/docs/containers/container-infinite-scroll.mdx?collection=docs&hash=1748883387721"
+import * as docs_8 from "../content/docs/containers/container-clipped.mdx?collection=docs&hash=1748883387721"
+import * as docs_7 from "../content/docs/cards/cards-stack.mdx?collection=docs&hash=1748883387721"
+import * as docs_6 from "../content/docs/cards/cards-stack-rotated.mdx?collection=docs&hash=1748883387721"
+import * as docs_5 from "../content/docs/cards/card-testimonial.mdx?collection=docs&hash=1748883387721"
+import * as docs_4 from "../content/docs/cards/card-hover-reveal.mdx?collection=docs&hash=1748883387721"
+import * as docs_3 from "../content/docs/cards/card-flip.mdx?collection=docs&hash=1748883387721"
+import * as docs_2 from "../content/docs/cards/card-curtain-reveal.mdx?collection=docs&hash=1748883387721"
+import * as docs_1 from "../content/docs/background/background-gradient.mdx?collection=docs&hash=1748883387721"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1748883387721"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"background/background-gradient.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/background/background-gradient.mdx"}, data: docs_1 }, { info: {"path":"cards/card-curtain-reveal.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-curtain-reveal.mdx"}, data: docs_2 }, { info: {"path":"cards/card-flip.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-flip.mdx"}, data: docs_3 }, { info: {"path":"cards/card-hover-reveal.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-hover-reveal.mdx"}, data: docs_4 }, { info: {"path":"cards/card-testimonial.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/card-testimonial.mdx"}, data: docs_5 }, { info: {"path":"cards/cards-stack-rotated.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/cards-stack-rotated.mdx"}, data: docs_6 }, { info: {"path":"cards/cards-stack.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/cards/cards-stack.mdx"}, data: docs_7 }, { info: {"path":"containers/container-clipped.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-clipped.mdx"}, data: docs_8 }, { info: {"path":"containers/container-infinite-scroll.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-infinite-scroll.mdx"}, data: docs_9 }, { info: {"path":"containers/container-stagger.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/container-stagger.mdx"}, data: docs_10 }, { info: {"path":"containers/gallery-rotated-scroll.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/gallery-rotated-scroll.mdx"}, data: docs_11 }, { info: {"path":"containers/grid-bento.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/grid-bento.mdx"}, data: docs_12 }, { info: {"path":"containers/grid-staggered.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/containers/grid-staggered.mdx"}, data: docs_13 }, { info: {"path":"custom-cursor/custom-cursor.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/custom-cursor/custom-cursor.mdx"}, data: docs_14 }, { info: {"path":"heros/hero-gradient.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/heros/hero-gradient.mdx"}, data: docs_15 }, { info: {"path":"heros/hero-rotated-gallery.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/heros/hero-rotated-gallery.mdx"}, data: docs_16 }, { info: {"path":"rating-stars/rating-stars.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/rating-stars/rating-stars.mdx"}, data: docs_17 }, { info: {"path":"section-gallery/section-gallery.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/section-gallery/section-gallery.mdx"}, data: docs_18 }, { info: {"path":"slideshow/slideshow.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/slideshow/slideshow.mdx"}, data: docs_19 }, { info: {"path":"story/story.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/story/story.mdx"}, data: docs_20 }, { info: {"path":"text/text-stagger-hover.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/text/text-stagger-hover.mdx"}, data: docs_21 }, { info: {"path":"text/text-stagger-inview.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/text/text-stagger-inview.mdx"}, data: docs_22 }, { info: {"path":"text/text-vertical.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/text/text-vertical.mdx"}, data: docs_23 }, { info: {"path":"utils/set-stagger-direction.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/utils/set-stagger-direction.mdx"}, data: docs_24 }, { info: {"path":"utils/use-animation-variants.mdx","absolutePath":"/Users/ycf/Documents/systaliko-ui/content/docs/utils/use-animation-variants.mdx"}, data: docs_25 }], [])
