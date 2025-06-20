3f8da1bf3b53c67c531ab76b308533304e17a8b3
Josef Bnm <ssefbnm@gmail.com>
2025-06-20 03:07:00 +0100

    fix component-installation syntax error

--- registry/demo/cards/cards-stack-sm/index.tsx
+++ registry/demo/cards/cards-stack-sm/index.tsx
@@ -5,25 +5,25 @@ const ACHIEVEMENTS = [
     id: 'achivement-1',
     title: '4',
     description: 'site of the day',
-    bg: 'var(--primary)',
+    bg: 'bg-indigo-500',
   },
   {
     id: 'achivement-2',
     title: '60+',
     description: 'website created',
-    bg: 'var(--chart-2)',
+    bg: 'bg-emerald-500',
   },
   {
     id: 'achivement-3',
     title: '5+',
     description: 'years of experience',
-    bg: 'var(--chart-1)',
+    bg: 'bg-pink-500',
   },
   {
     id: 'achivement-4',
     title: '6+',
     description: 'component created',
-    bg: 'var(--chart-4)',
+    bg: 'bg-purple-500',
   },
 ];
 
@@ -35,8 +35,8 @@ export function CardsStackSmDemo() {
           key={achievement.id}
           incrementY={20}
           index={index + 2}
-          className="flex h-72 w-[420px] flex-col place-content-center justify-evenly rounded-2xl  p-8 shadow-md"
-          style={{ rotate: index + 2, background: achievement.bg }}
+          className={`flex ${achievement.bg} text-white h-72 w-[420px] flex-col place-content-center justify-evenly rounded-2xl  p-8 shadow-md`}
+          style={{ rotate: index + 2 }}
         >
           <h1 className="text-left text-6xl font-semibold opacity-80">
             {achievement.title}
