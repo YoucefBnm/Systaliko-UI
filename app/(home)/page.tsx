59cbc7d43d95942cb2f02407e353bbf0e25fad8b
Josef Bnm <ssefbnm@gmail.com>
2025-06-30 16:36:50 +0100

    sync pnpm

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,15 +2,13 @@
 import { Features } from '@/components/features';
 import { Header } from '@/components/header';
 import { Hero } from '@/components/hero';
-import { AgencyPageDemo } from '@/registry/demo/pages/agency';
 
 export default function HomePage() {
   return (
     <>
-      {/* <Header /> */}
-      <AgencyPageDemo />
-      {/* <Hero />
-      <Features /> */}
+      <Header />
+      <Hero />
+      <Features />
     </>
   );
 }
