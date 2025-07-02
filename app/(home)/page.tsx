01c383aaeda113121291553d4b26c1b756831343
Josef Bnm <ssefbnm@gmail.com>
2025-07-02 01:48:34 +0100

    add scroll-x-carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,15 +2,13 @@
 import { Features } from '@/components/features';
 import { Header } from '@/components/header';
 import { Hero } from '@/components/hero';
-import { ScrollXCarouselDemo } from '@/registry/demo/scroll-x-carousel';
 
 export default function HomePage() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Hero />
-      <Features /> */}
-      <ScrollXCarouselDemo />
+      <Features />
     </>
   );
 }
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -6,7 +6,7 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <div className="inline-flex items-center ">
         <Logo className="h-auto w-32" />{' '}
-        <sup className="text-[10px] leading-tight bg-foreground/80 text-background px-1.5  rounded-full">
+        <sup className="text-[10px] font-semibold uppercase leading-tight bg-foreground/85 text-background py-0.5 px-1.5  rounded-sm">
           Beta
         </sup>
       </div>
