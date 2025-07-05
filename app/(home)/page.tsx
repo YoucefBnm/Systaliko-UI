dd83f5e0a5c861c820c4de5d600aa994830ae837
Josef Bnm <ssefbnm@gmail.com>
2025-07-05 01:40:09 +0100

    fix grid-staggered

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,14 +2,12 @@
 import { Features } from '@/components/features';
 import { Header } from '@/components/header';
 import { Hero } from '@/components/hero';
-import { SectionVideoDemo } from '@/registry/demo/section-video';
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <Hero />
-      <SectionVideoDemo />
       <Features />
     </>
   );
