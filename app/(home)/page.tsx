98f70cd99efc0406fb6534ebcaaa5e2121ab08ad
Josef Bnm <ssefbnm@gmail.com>
2025-09-03 00:31:32 +0100

    remove team section from home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,14 +3,12 @@ import { Footer } from '@/components/sections/footer';
 import { Hero } from '@/components/sections/hero';
 // import { HomeBanner } from '@/components/sections/home-banner';
 import { HomeNav } from '@/components/sections/home-nav';
-import { Team } from '@/registry/demo/blocks/team';
 
 export default function HomePage() {
   return (
     <>
       {/* <HomeBanner /> */}
       <HomeNav />
-      <Team />
       <Hero />
       <Features />
       <Footer />
